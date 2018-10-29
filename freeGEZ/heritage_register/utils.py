303fcf78b0fa04e967bdecd374df2bd97fc1f1cc
rkucmierowski <r.kucmierowski@interia.pl>
2018-10-29 18:37:33 +0100

    Try easy_pdf - unsupported external css

--- freeGEZ/heritage_register/utils.py
+++ freeGEZ/heritage_register/utils.py
@@ -1,14 +1,2 @@
-from io import BytesIO
-from django.http import HttpResponse
-from django.template.loader import get_template
-from xhtml2pdf import pisa
-
-
-def render_to_pdf(template_src, ctx={}):
-    template = get_template(template_src)
-    html = template.render(ctx)
-    result = BytesIO
-    pdf = pisa.pisaDocument(BytesIO(html.encode("utf-8")), result)
-    if not pdf.err:
-        return HttpResponse(result.getvalue(), content_type='application/pdf')
-    return None
+from django.conf import settings
+from easy_pdf.views import PDFTemplateView

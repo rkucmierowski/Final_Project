0c5527df7f05f3781d89e958f43e200dc9ad8e66
rkucmierowski <r.kucmierowski@interia.pl>
2018-10-29 18:30:33 +0100

    Try xhtml2pdf - TypeError

--- freeGEZ/heritage_register/views.py
+++ freeGEZ/heritage_register/views.py
@@ -1,5 +1,19 @@
+import datetime
+
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import View
+from django.template.loader import get_template
+
+from .utils import render_to_pdf
 
 def base(request):
     if request.method == 'GET':
-        return render(request, "heritage_register/card_pattern.html", {})
+        return render(request, 'heritage_register/card_pattern.html', {})
+
+class GeneratePdf(View):
+    def get(self, request):
+        pdf = render_to_pdf('heritage_register/card_pattern.html',{})
+        if pdf:
+            return HttpResponse(pdf, content_type='application/pdf')
+        return HttpResponse("Not not not")

e5e406c3b164665589d2d5be8f9e613a67b1ba77
rkucmierowski <r.kucmierowski@interia.pl>
2018-10-29 17:22:02 +0100

    Add static dir

--- freeGEZ/heritage_register/views.py
+++ freeGEZ/heritage_register/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+def base(request):
+    if request.method == 'GET':
+        return render(request, "heritage_register/card_pattern.html", {})

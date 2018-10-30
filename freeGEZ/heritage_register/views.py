08d56dd40e888d2ea0af61971ccbe2c53c57411f
rkucmierowski <r.kucmierowski@interia.pl>
2018-10-30 17:22:04 +0100

    Correct v-align, separate print css

--- freeGEZ/heritage_register/views.py
+++ freeGEZ/heritage_register/views.py
@@ -14,12 +14,16 @@ def base(request):
 class GeneratePdf(View):
 
     def get(self, request):
-        rules=''
-        with open('{}/static/heritage_register/style.css'.format(BASE_DIR)) as f2:
-            rules = f2.read()
-        html = HTML(filename='{}/templates/heritage_register/card_pattern.html'.format(BASE_DIR))
-        font_conf = FontConfiguration()
-        css = CSS(string=rules,  font_config=font_conf)
-        pdf = html.write_pdf(stylesheets=[css], font_config=font_conf)
+        # rules=''
+        # with open('{}/static/heritage_register/style.css'.format(BASE_DIR)) as f2:
+        #     rules = f2.read()
+        # html = HTML(filename='{}/templates/heritage_register/card_pattern.html'.format(BASE_DIR))
+        # font_conf = FontConfiguration()
+        # css = CSS(string=rules,  font_config=font_conf)
+        # pdf = html.write_pdf(stylesheets=[css], font_config=font_conf)
+        # return HttpResponse(pdf, content_type='application/pdf')
+
+        html = HTML(url='http://localhost:8000/')
+        pdf = html.write_pdf()
         return HttpResponse(pdf, content_type='application/pdf')
 
